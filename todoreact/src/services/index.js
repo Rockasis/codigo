15e1c90b40140aab4bd4d4566bd5c27ef8c0e251
unknown <alfredo.rocca22@gmail.com>
2022-11-03 20:00:05 -0500

    actualizando..

---
+++ todoreact/src/services/index.js
@@ -0,0 +1,49 @@
+const url = "https://63645b008a3337d9a2f5dd45.mockapi.io/tareasgit";
+
+// GET : Listar
+// POST: Crear
+// PUT: Actualizar
+// DELETE: Eliminar
+
+export const get = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const post = async (body) => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// id: Tarea
+export const update = async (id, body) => {
+  try {
+    const response = await fetch(`${url}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
