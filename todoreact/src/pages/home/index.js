fcc2d43d59c6e2653085deb6469a25f5caa51aed
unknown <alfredo.rocca22@gmail.com>
2022-11-10 21:59:29 -0500

    feat: context

--- todoreact/src/pages/home/index.js
+++ todoreact/src/pages/home/index.js
@@ -1,23 +1,35 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { TaskForm, TaskCard } from "../../components";
 import { get, post, update } from "../../services";
+import { TaskModel } from "../../models/TaskModel";
 
 function Home() {
   const [taskList, setTaskList] = useState([]);
 
   async function getTasks() {
     const tasks = await get();
-    setTaskList(tasks);
+    const tasksModels = tasks.map((task) => {
+      return new TaskModel(
+        task.id,
+        task.name,
+        task.createdAt,
+        task.doneAt,
+        task.deletedAt
+      );
+    });
+    setTaskList(tasksModels);
   }
 
   async function addTask(text) {
-    const newTask = { name: text, status: 1 };
+    const newTask = new TaskModel(null, text);
     await post(newTask);
     await getTasks();
   }
 
-  async function updateTask(id) {
-    const body = { status: 2 };
+  async function updateTask(id, type) {
+    const body =
+      type === "done" ? { doneAt: new Date() } : { deletedAt: new Date() };
     await update(id, body);
     await getTasks();
   }
@@ -30,6 +42,9 @@ function Home() {
   return (
     <div className="container my-5">
       <h1 className="display-3">Todo App</h1>
+      <Link to="/perfil" className="my-3 btn btn-link">
+        Ir a perfil
+      </Link>
       <TaskForm onSubmitFunction={addTask} />
       <div className="text-primary">
         <hr />
